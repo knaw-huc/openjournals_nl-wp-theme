36846eefafa20aca09bd33b355977aeeaa6d2cbc
Bas Doppen <basdoppen@gmail.com>
2022-01-20 12:49:31 +0100

    Newsletter signup

--- home.php
+++ home.php
@@ -63,6 +63,10 @@
       </div>
       <div class="topLine">
         <h2>Newsletter</h2>
+        <?php wp_reset_query(); query_posts( 'pagename=newsletter_subscribtion&post_type=page' ); ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+          <?php the_content(); ?>
+        <?php endwhile; ?>
       </div>
       <div class="topLine">
         <h2>Partners</h2>
